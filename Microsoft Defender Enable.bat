f82e1a1b945e8859c121287f2ea27838813d4009
Eduard <handsomefox.hf@gmail.com>
2020-09-07 09:34:33 +0300

    Initial Stuff

---
+++ Microsoft Defender Enable.bat
@@ -0,0 +1,26 @@
+rem run this twice with admin rights
+
+rem Enable Tamper Protection back
+reg add "HKLM\Software\Microsoft\Windows Defender\Features" /v "TamperProtection" /t REG_DWORD /d "1" /f
+
+rem Remove all the policies set before
+reg delete "HKLM\Software\Policies\Microsoft\Windows Defender" /f
+
+rem Enable Logging back
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderApiLogger" /v "Start" /t REG_DWORD /d "1" /f
+reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "1" /f
+
+rem Reverse disable WD Tasks
+schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Enable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable
+
+rem Enable WD services (AutoStart)
+reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "2" /f
+
+pause
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# disable-windefend
+# Disable WinDefend
+
+<p align="center">
+<img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Windows_Defender_logo.svg">
+
+[Reason why this exists](https://pureinfotech.com/windows-10-removes-disableantispyware-disable-defender/)
+
+</p>
+
+## Instructions
+
+1. Disable Tamper Protection manually before running.
+
+2. Run script with admin rights twice to disable/enable everything.
+
+## Some additional stuff you may want
+
+### Delete Systray icon
+
+```batchfile
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
+```
+
+### Delete Context menu entries
+
+```batchfile
+reg delete "HKCR\*\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /f
+reg delete "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /f
+```
+
+### Disable System Guard Runtime Monitor Broker
+
+```batchfile
+reg add "HKLM\System\CurrentControlSet\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
+```
+
+### Disable Windows Defender Security Center
+
+```batchfile
+reg add "HKLM\System\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d "4" /f
+```
