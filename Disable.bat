acdca6db846459ed768464db76f5a389692544bb
handsomefox <handsomefox.hf@gmail.com>
2021-02-22 02:23:13 +0200

    Exit after script execution

--- Disable.bat
+++ Disable.bat
@@ -35,3 +35,4 @@ reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWOR
 reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
+exit
